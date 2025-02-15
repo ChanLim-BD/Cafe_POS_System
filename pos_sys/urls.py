c593df3db08a13693beacb233d0b9b2fe963e4e6
ChanLim-BD <linsay070@gmail.com>
2025-02-15 06:36:57 +0000

    Custom User Model 설정 (전화번호 중심)

---
+++ pos_sys/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")), # auth 테스트
+    path('accounts/', include('accounts.urls')),
+]
