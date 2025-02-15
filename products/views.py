56d2d3daf8dde6ecc1dde5d311226abdfd18e9fe
ChanLim-BD <linsay070@gmail.com>
2025-02-15 10:59:25 +0000

    POS 사용자는 모두 사장이므로, 로그인한 대상 모두 허용

--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
-from rest_framework import generics
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from products.models import Product
 from products.serializers import ProductSerializer
 
 
 # 상품 등록 (POST)
-class ProductCreateView(generics.CreateAPIView):
+class ProductCreateView(CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]  # 로그인 필수
@@ -15,16 +15,16 @@ class ProductCreateView(generics.CreateAPIView):
 
 
 # 상품 리스트 조회 (GET)
-class ProductListView(generics.ListAPIView):
+class ProductListView(ListAPIView):
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]  # 로그인 필수
 
     def get_queryset(self):
-        return Product.objects.filter(owner=self.request.user)  # 본인이 등록한 상품만 조회
+        return Product.objects.all()  # 모든 상품 조회 (소유자 관계 없음)
 
 
 # 상품 부분 수정 (PATCH) & 상세 조회 (GET)
-class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+class ProductDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]  # 로그인 필수 
